7b6217ccfa0e04af8671b0165d5ee1bb297135f8
アントニオ <arqueiroverd@gmail.com>
2025-10-03 21:22:02 -0300

    test(createcategoryusecase): should pass the  category to the repository

--- src/SpendTracker.Application/Categories/Add/ICategoryRepository.cs
+++ src/SpendTracker.Application/Categories/Add/ICategoryRepository.cs
@@ -5,4 +5,5 @@ namespace SpendTracker.Application.Categories.Add;
 public interface ICategoryRepository
 {
     Task<bool> HasCategoryWithNameAsync(string name);
+    Task<Category> AddAsync(Category category);
 }
--- tests/SpendTracker.Application.Tests/Categories/Add/CategoryUseCaseTests.cs
+++ tests/SpendTracker.Application.Tests/Categories/Add/CategoryUseCaseTests.cs
@@ -9,54 +9,73 @@ public class CategoryUseCaseTests
     private readonly Faker _faker = FakerHelper.Faker;
     private readonly ICategoryRepository _categoryRepositoryMock;
     private readonly CreateCategoryUseCase _sut;
+
+    private readonly string _name;
+    private readonly string _description;
+    private readonly CreateCategoryCommand _command ;
+    private CreateCategoryCommand GenerateCommand() => new(_name, _description);
+
+    
     public CategoryUseCaseTests()
     { 
+         _name = _faker.Commerce.Categories(1)[0]; 
+        _description = _faker.Lorem.Sentence();
+        _command = GenerateCommand();
+        
         _categoryRepositoryMock = Substitute.For<ICategoryRepository>();
+        _categoryRepositoryMock.HasCategoryWithNameAsync(_command.Name).Returns(false);
         _sut = new CreateCategoryUseCase(_categoryRepositoryMock);
+        
     }
 
-    private CreateCategoryCommand GenerateCommand()
-    {
-        var name = _faker.Name.FirstName();
-        var description = _faker.Commerce.ProductName();
-        return new CreateCategoryCommand(name, description);
-    }
 
     [Fact] public async Task  ShouldInvokeHasCategoryWithNameAsyncOnceAndReturnResult_WhenCategoryDoesNotExist()
     {
-        // Arrange
-        var command = GenerateCommand();
+        
+        //Act
+        var result = await _sut.Perform(_command);
 
-        _categoryRepositoryMock
-            .HasCategoryWithNameAsync(command.Name)
-            .Returns(Task.FromResult(false));
-
-        // Act
-        var result = await _sut.Perform(command);
-
-        // Assert
-        await _categoryRepositoryMock.Received(1).HasCategoryWithNameAsync(command.Name);
+        //Assert
+        await _categoryRepositoryMock.Received(1).HasCategoryWithNameAsync(_command.Name);
         result.IsSuccess.ShouldBeTrue();
         result.Value.Id.ShouldNotBe(Guid.Empty);
-        result.Value.Name.ShouldBe(command.Name);
-        result.Value.Description.ShouldBe(command.Description);
+        result.Value.Name.ShouldBe(_command.Name);
+        result.Value.Description.ShouldBe(_command.Description);
     }
 
     [Fact]
     public async Task ShouldFailWhenCategoryAlreadyExists()
     {
         // Arrange
-        var command = GenerateCommand();
         _categoryRepositoryMock
-            .HasCategoryWithNameAsync(command.Name)
+            .HasCategoryWithNameAsync(_command.Name)
             .Returns(Task.FromResult(true));
 
-        // Act
-        var result = await _sut.Perform(command);
+        //Act
+        var result = await _sut.Perform(_command);
         
-        // Assert
-        await _categoryRepositoryMock.Received(1).HasCategoryWithNameAsync(command.Name);
+        //Assert
+        await _categoryRepositoryMock.Received(1).HasCategoryWithNameAsync(_command.Name);
         result.IsFailure.ShouldBeTrue();
         result.Error.ShouldBe(CategoryErrors.CategoryNameAlreadyExists);
+    }
+
+    [Fact]
+    public async Task ShouldInstantiateCategoryAndPassItToTheRepository()
+    {
+        // Arrange
+        var name = $"  {_name}  "; 
+        var command = new CreateCategoryCommand(name, _description);
+        // Act
+        var result = await _sut.Perform(command);
+        var expectedName = command.Name.Trim();
+        // Assert
+        result.IsSuccess.ShouldBeTrue();
+
+        await _categoryRepositoryMock.Received(1).AddAsync(Arg.Is<Category>(category =>
+            category.Name == expectedName &&
+            category.Description == command.Description &&
+            category.Id != Guid.Empty
+        ));
     }
 }
