1088ef384ab755ca2f01855a939a0335d3ba60f7
アントニオ <arqueiroverd@gmail.com>
2025-11-03 20:56:43 -0300

    feat: if the category does not exist, return empty list

---
+++ src/WebApi/Features/Categories/GetAll/GetAllCategoriesUseCase.cs
@@ -0,0 +1,21 @@
+﻿using SharedKernel.Abstractions;
+using WebApi.Features.Categories.Common;
+
+namespace WebApi.Features.Categories.GetAll;
+
+public interface IGetAllCategoriesUseCase : IUseCaseWithoutInput<Task<IEnumerable<CategoryDto>>>;
+
+internal class GetAllCategoriesUseCase : IGetAllCategoriesUseCase
+{
+    private readonly ICategoryRepository _repo;
+
+    public GetAllCategoriesUseCase(ICategoryRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task<IEnumerable<CategoryDto>> Perform()
+    {
+        return await _repo.GetAllAsync();
+    }
+}
