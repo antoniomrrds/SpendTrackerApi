f5dc84bbaf5b4835a674938a58c70027d6d19839
アントニオ <arqueiroverd@gmail.com>
2025-11-03 20:57:26 -0300

    test: if the category does not exist, return empty list

---
+++ tests/WebApi.Tests/Features/Categories/GetAll/GetAllCategoriesUseCaseTests.cs
@@ -0,0 +1,34 @@
+﻿using NSubstitute;
+using TestUtilities.Common;
+using WebApi.Features.Categories.Common;
+using WebApi.Features.Categories.GetAll;
+using WebApi.Tests.Features.Categories.Common;
+
+namespace WebApi.Tests.Features.Categories.GetAll;
+
+[Trait("Type", "Unit")]
+public class GetAllCategoriesUseCaseTests : TestCommon
+{
+    private readonly IEnumerable<CategoryDto> _categoryDto = CategoryDtoMock.ValidList();
+    private readonly IEnumerable<CategoryDto> _emptyCategory = CategoryDtoMock.EmptyList();
+    private readonly GetAllCategoriesUseCase _sut;
+    private readonly ICategoryRepository _categoryRepositoryMock;
+
+    public GetAllCategoriesUseCaseTests()
+    {
+        _categoryRepositoryMock = Substitute.For<ICategoryRepository>();
+        _categoryRepositoryMock.GetAllAsync(AnyCancellationToken).Returns(_categoryDto);
+        _sut = new GetAllCategoriesUseCase(_categoryRepositoryMock);
+    }
+
+    [Fact]
+    public async Task Perform_WhenCategoryDoesNotExist_ShouldReturnEmptyList()
+    {
+        //Arrange
+        _categoryRepositoryMock.GetAllAsync(AnyCancellationToken).Returns(_emptyCategory);
+        //Action
+        IEnumerable<CategoryDto> result = await _sut.Perform();
+        //Assert
+        result.ShouldBe([]);
+    }
+}
