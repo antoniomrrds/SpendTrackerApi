9c0b476e11415471992090d75a0fc5a8a979a094
アントニオ <arqueiroverd@gmail.com>
2025-11-05 12:18:07 -0300

    test: add E2E test for GetAllCategories endpoint

---
+++ tests/WebApi.Tests/Features/Categories/GetAll/GetAllCategoriesHelper.cs
@@ -0,0 +1,34 @@
+﻿using WebApi.Common.Web.Responses.Success;
+using WebApi.Features.Categories.Common;
+using WebApi.Tests.Helpers.Extensions;
+
+namespace WebApi.Tests.Features.Categories.GetAll;
+
+internal static class GetAllCategoriesHelper
+{
+    public static async Task<HttpResponseMessage> GetAllCategories(
+        this HttpClient client,
+        CancellationToken cancellationToken
+    )
+    {
+        HttpResponseMessage response = await client.GetAsync(
+            CategoriesRoutes.GetAll,
+            cancellationToken
+        );
+        return response;
+    }
+
+    public static async Task<IReadOnlyList<CategoryDto>> GetAllCategoriesAndReturnDto(
+        this HttpClient client,
+        CancellationToken cancellationToken
+    )
+    {
+        HttpResponseMessage response = await client.GetAllCategories(cancellationToken);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        ApiSuccessResponse<IReadOnlyList<CategoryDto>> apiResponse = await response.GetApiResponse<
+            IReadOnlyList<CategoryDto>
+        >();
+        apiResponse.Payload.ShouldNotBeNull();
+        return apiResponse.Payload;
+    }
+}
---
+++ tests/WebApi.Tests/Features/Categories/GetAll/GetAllCategoriesTests.cs
@@ -0,0 +1,42 @@
+﻿using WebApi.Domain.Categories;
+using WebApi.Features.Categories.Common;
+using WebApi.Tests.Domain.Categories;
+using WebApi.Tests.Helpers.Factories;
+
+namespace WebApi.Tests.Features.Categories.GetAll;
+
+[Trait("Type", "E2E")]
+public class GetAllCategoriesTests : BaseIntegrationTest<SqliteTestWebAppFactory>
+{
+    private readonly List<Category> _categories;
+
+    public GetAllCategoriesTests(SqliteTestWebAppFactory factory)
+        : base(factory)
+    {
+        _categories = CategoryFixture.GetCategories(3, true);
+    }
+
+    private async Task SeedCategoriesAsync()
+    {
+        await DbContext.Categories.AddRangeAsync(_categories);
+        await DbContext.SaveChangesAsync();
+    }
+
+    [Fact]
+    public async Task GetAllCategories_WhenCategoryExists_ReturnsCategoryDto()
+    {
+        //Arrange
+        await SeedCategoriesAsync();
+        //Action
+        IReadOnlyList<CategoryDto> response = await HttpClient.GetAllCategoriesAndReturnDto(
+            CancellationToken
+        );
+        //Assert
+        response.Count.ShouldBe(_categories.Count);
+        response.ShouldAllBe(dto =>
+            _categories.Any(c =>
+                c.Id == dto.Id && c.Name == dto.Name && c.Description == dto.Description
+            )
+        );
+    }
+}
