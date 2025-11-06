7e91ab447a03481fff8435824099c54ab10ec3de
アントニオ <arqueiroverd@gmail.com>
2025-11-06 14:57:55 -0300

    test: add UpdateCategoryValidator and all tests

---
+++ tests/WebApi.Tests/Features/Categories/Update/UpdateCategoryFixture.cs
@@ -0,0 +1,18 @@
+﻿using WebApi.Domain.Categories;
+using WebApi.Features.Categories.Update;
+using WebApi.Tests.Domain.Categories;
+
+namespace WebApi.Tests.Features.Categories.Update;
+
+public static class UpdateCategoryFixture
+{
+    public static UpdateCategoryInput UpdateInput(bool useNewSeed = false)
+    {
+        Category category = CategoryFixture.GetCategory(useNewSeed);
+        return new UpdateCategoryInput()
+        {
+            Name = category.Name,
+            Description = category.Description,
+        };
+    }
+}
--- tests/WebApi.Tests/Features/Categories/Update/UpdateCategoryValidatorTests.cs
+++ tests/WebApi.Tests/Features/Categories/Update/UpdateCategoryValidatorTests.cs
@@ -1,10 +1,26 @@
-﻿// using TestUtilities.Common;
-//
-// namespace WebApi.Tests.Features.Categories.Update;
-//
-// internal class UpdateCategoryValidatorTests
-// {
-//     public UpdateCategoryValidatorTests()
-//     {
-//     }
-// }
+﻿using FluentValidation.TestHelper;
+using WebApi.Features.Categories.Update;
+using WebApi.Tests.Features.Categories.Common;
+
+namespace WebApi.Tests.Features.Categories.Update;
+
+[Trait("Type", "Unit")]
+public class UpdateCategoryValidatorTests
+    : CategoryValidatorTestBase<UpdateCategoryValidator, UpdateCategoryInput>
+{
+    private readonly UpdateCategoryInput _updateInput = UpdateCategoryFixture.UpdateInput();
+
+    protected override UpdateCategoryInput DefaultInput() =>
+        new() { Name = _updateInput.Name, Description = _updateInput.Description };
+
+    [Fact]
+    public void Validator_WhenInputIsFullyValid_ShouldPassAllValidations()
+    {
+        // Arrange
+        UpdateCategoryInput input = DefaultInput();
+        // Act
+        TestValidationResult<UpdateCategoryInput>? result = Sut.TestValidate(input);
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
