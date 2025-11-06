4f76019028a21dc1515dbf81cc64ab13e7a94f77
アントニオ <arqueiroverd@gmail.com>
2025-11-06 14:57:36 -0300

    feat: add UpdateCategoryValidator and all tests

---
+++ src/WebApi/Features/Categories/Update/UpdateCategoryUseCase.cs
@@ -0,0 +1,16 @@
+﻿using SharedKernel.Abstractions;
+using WebApi.Features.Categories.Common;
+
+namespace WebApi.Features.Categories.Update;
+
+public interface IUpdateCategoryUseCase : IUseCase<UpdateCategoryInput, Task<Result<CategoryDto>>>;
+
+public sealed record UpdateCategoryInput : CommonCategoryProperties;
+
+internal class UpdateCategoryUseCase : IUpdateCategoryUseCase
+{
+    public Task<Result<CategoryDto>> Perform(UpdateCategoryInput input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/WebApi/Features/Categories/Update/UpdateCategoryValidator.cs
@@ -0,0 +1,5 @@
+﻿using WebApi.Features.Categories.Common;
+
+namespace WebApi.Features.Categories.Update;
+
+public class UpdateCategoryValidator : CommonCategoryValidator<UpdateCategoryInput>;
