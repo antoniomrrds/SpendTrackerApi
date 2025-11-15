b643b62f7ee3d43f179f26977a3ec8ea7e833e06
アントニオ <arqueiroverd@gmail.com>
2025-11-15 09:10:06 -0300

    test: ensure DeleteCategoryUseCase returns failure when category does not exist

---
+++ tests/WebApi.Tests/Features/Categories/Delete/DeleteCategoryFixture.cs
@@ -0,0 +1,14 @@
+﻿using WebApi.Domain.Categories;
+using WebApi.Features.Categories.Delete;
+using WebApi.Tests.Domain.Categories;
+
+namespace WebApi.Tests.Features.Categories.Delete;
+
+internal static class DeleteCategoryFixture
+{
+    public static DeleteCategoryInput DeleteInput(bool useNewSeed = false)
+    {
+        Category category = CategoryFixture.GetCategory(useNewSeed);
+        return new DeleteCategoryInput() { Id = category.Id };
+    }
+}
---
+++ tests/WebApi.Tests/Features/Categories/Delete/DeleteCategoryUseCaseTests.cs
@@ -0,0 +1,45 @@
+﻿using NSubstitute;
+using SharedKernel;
+using WebApi.Domain.Categories;
+using WebApi.Features.Categories.Common;
+using WebApi.Features.Categories.Delete;
+
+namespace WebApi.Tests.Features.Categories.Delete;
+
+[Trait("Type", "Integration")]
+public class DeleteCategoryUseCaseTests
+{
+    private readonly DeleteCategoryUseCase _sut;
+    private readonly ICategoryWriterRepository _categoryWriterRepository;
+    private readonly CancellationToken _ct = CancellationToken.None;
+    private readonly DeleteCategoryInput _input = DeleteCategoryFixture.DeleteInput();
+
+    public DeleteCategoryUseCaseTests()
+    {
+        _categoryWriterRepository = Substitute.For<ICategoryWriterRepository>();
+
+        _sut = new DeleteCategoryUseCase(_categoryWriterRepository);
+    }
+
+    [Fact]
+    public async Task Perform_WhenCategoryToDeleteIsNotFound_ShouldReturnFailure()
+    {
+        //Arrange
+        MakeDeleteAsyncReturns(false);
+        //Act
+        Result<bool> result = await _sut.Perform(input: _input, cancellationToken: _ct);
+        //Assert
+        await _categoryWriterRepository
+            .Received(1)
+            .DeleteAsync(id: _input.Id, cancellationToken: _ct);
+        result.IsFailure.ShouldBeTrue();
+        result.Error.ShouldBe(CategoryErrors.NotFound(_input.Id.ToString()));
+    }
+
+    private void MakeDeleteAsyncReturns(bool returnValue = true)
+    {
+        _categoryWriterRepository
+            .DeleteAsync(id: _input.Id, cancellationToken: _ct)
+            .Returns(returnValue);
+    }
+}
