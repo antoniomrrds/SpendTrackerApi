0f671a8fe4c10baf24479d5802fa45a1e1f62933
アントニオ <arqueiroverd@gmail.com>
2025-11-20 07:26:07 -0300

    test(CreateExpenseUseCase): esure that IUnitOfWork persists an expense

--- tests/WebApi.Tests/Features/Expenses/Create/CreateExpenseUseCaseTests.cs
+++ tests/WebApi.Tests/Features/Expenses/Create/CreateExpenseUseCaseTests.cs
@@ -1,5 +1,6 @@
 ﻿using NSubstitute;
 using SharedKernel;
+using SharedKernel.Abstractions.Data;
 using TestUtilities.Common;
 using WebApi.Domain.Categories;
 using WebApi.Domain.Expenses;
@@ -17,23 +18,25 @@ public class CreateExpenseUseCaseTests : TestCommon
     private readonly CreateExpenseInput _input = CreateExpenseFixture.ExpenseInput();
     private readonly ICategoryCheckRepository _categoryCheckRepo;
     private readonly IExpenseWriterRepository _expenseWriterRepo;
+    private readonly IUnitOfWork _unitOfWork;
 
     public CreateExpenseUseCaseTests()
     {
         _categoryCheckRepo = Substitute.For<ICategoryCheckRepository>();
         _expenseWriterRepo = Substitute.For<IExpenseWriterRepository>();
-        _sut = new CreateExpenseUseCase(_categoryCheckRepo, _expenseWriterRepo);
+        _unitOfWork = Substitute.For<IUnitOfWork>();
+        _sut = new CreateExpenseUseCase(_categoryCheckRepo, _expenseWriterRepo, _unitOfWork);
     }
 
     [Fact]
     public async Task Perform_WhenCategoryDoesNotExist_ShouldReturnFailureWithProperError()
     {
         //Arrange
-        _categoryCheckRepo.CategoryExists(_input.IdCategory, Ct).Returns(false);
+        MakeCategoryExistsAsyncReturns(false);
         //Act
         Result<bool> result = await _sut.Perform(_input, Ct);
         //Assert
-        await _categoryCheckRepo.Received(1).CategoryExists(_input.IdCategory, Ct);
+        await _categoryCheckRepo.Received(1).CategoryExistsAsync(_input.IdCategory, Ct);
         result.IsFailure.ShouldBeTrue();
         result.Error.ShouldBe(CategoryErrors.NotFound(_input.IdCategory.ToString()));
     }
@@ -42,11 +45,29 @@ public class CreateExpenseUseCaseTests : TestCommon
     public async Task Perform_WhenCategoryExists_ShouldAddExpense()
     {
         //Arrange
-        _categoryCheckRepo.CategoryExists(_input.IdCategory, Ct).Returns(true);
+        MakeCategoryExistsAsyncReturns();
         //Act
         Result<bool> result = await _sut.Perform(_input, Ct);
         //Assert
         await _expenseWriterRepo.Received(1).AddAsync(expense: AnyParameterForMock<Expense>(), Ct);
         result.IsSuccess.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task Perform_WhenAddExpense_ShouldCommitIUnitOfWork()
+    {
+        //Arrange
+        MakeCategoryExistsAsyncReturns();
+        //Act
+        Result<bool> result = await _sut.Perform(_input, Ct);
+        //Assert
+        await _expenseWriterRepo.Received(1).AddAsync(expense: AnyParameterForMock<Expense>(), Ct);
+        result.IsSuccess.ShouldBeTrue();
+        await _unitOfWork.Received(1).CommitAsync(cancellationToken: Ct);
+    }
+
+    private void MakeCategoryExistsAsyncReturns(bool returnValue = true)
+    {
+        _categoryCheckRepo.CategoryExistsAsync(_input.IdCategory, Ct).Returns(returnValue);
     }
 }
