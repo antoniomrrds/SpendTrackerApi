71a51f2c0e49ca44e2d0e1585bb922d58d887430
アントニオ <arqueiroverd@gmail.com>
2025-11-21 13:06:25 -0300

    test(CreateExpenseValidator): validate empty description returns failure

---
+++ tests/WebApi.Tests/Features/Expenses/Common/ExpenseValidatorTestBase.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using FluentValidation.TestHelper;
+using WebApi.Domain.Extensions;
+using WebApi.Domain.Resources;
+using WebApi.Features.Expenses.Common;
+
+namespace WebApi.Tests.Features.Expenses.Common;
+
+public abstract class ExpenseValidatorTestBase<TValidator, TInput>
+    where TValidator : IValidator<TInput>, new()
+    where TInput : CommonExpenseProperties, new()
+{
+    protected static TValidator Sut => new();
+    protected abstract TInput DefaultInput();
+
+    protected TInput BuildCommand(Action<TInput>? configure = null)
+    {
+        TInput input = DefaultInput();
+        configure?.Invoke(input);
+        return input;
+    }
+
+    [Fact]
+    public void Validator_WhenDescriptionIsEmpty_ShouldReturnError()
+    {
+        // Arrange
+        TInput input = BuildCommand(c => c.Description = string.Empty);
+        // Act
+        TestValidationResult<TInput>? result = Sut.TestValidate(input);
+
+        // Assert
+        result
+            .ShouldHaveValidationErrorFor(c => c.Description)
+            .WithErrorMessage(ValidationMessages.RequiredField.FormatInvariant("Description"));
+    }
+}
---
+++ tests/WebApi.Tests/Features/Expenses/Create/CreateExpenseValidatorTests.cs
@@ -0,0 +1,20 @@
+﻿using WebApi.Features.Expenses.Create;
+using WebApi.Tests.Features.Expenses.Common;
+
+namespace WebApi.Tests.Features.Expenses.Create;
+
+[Trait("Type", "Unit")]
+public class CreateExpenseValidatorTests
+    : ExpenseValidatorTestBase<CreateExpenseValidator, CreateExpenseInput>
+{
+    private readonly CreateExpenseInput _createExpenseInput = CreateExpenseFixture.ExpenseInput();
+
+    protected override CreateExpenseInput DefaultInput() =>
+        new()
+        {
+            Description = _createExpenseInput.Description,
+            Amount = _createExpenseInput.Amount,
+            Date = _createExpenseInput.Date,
+            IdCategory = _createExpenseInput.IdCategory,
+        };
+}
